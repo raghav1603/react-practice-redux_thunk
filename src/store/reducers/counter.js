8e1248d9a14ab12aacbe5ba391069c9e5c6270a9
RAGHAV 1 <raghavmishra.1603@gmail.com>
2020-07-24 00:55:41 +0530

    final commit

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,23 +1,31 @@
 import * as actionTypes from '../actions/actionType';
+import { updateObject } from '../utility'
 const initialState = {
     counter: 0
 }
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.INCREMENT:
-            return { ...state, counter: state.counter + 1 }
+            // return { ...state, counter: state.counter + 1  }             //this was to be used without the utility.js , not this function is hadled by that file to make the code look leaner
+            // const newstate= Object.assign({},state);
+            // newstate.counter=state.counter+1;
+            // return newstate
+            return updateObject(state, { counter: state.counter + 1 })
         // break;
         case actionTypes.DECREMENT:
-            return { ...state, counter: state.counter - 1 }
+            // return { ...state, counter: state.counter - 1 }
+            return updateObject(state, { counter: state.counter - 1 })
         // break;
         case actionTypes.ADD:
-            return { ...state, counter: state.counter + action.value }
+            // return { ...state, counter: state.counter + action.value }
+            return updateObject(state, { counter: state.counter + action.value })
         // break;
         case actionTypes.SUB:
-            return { ...state, counter: state.counter - action.value }
+            return updateObject(state, { counter: state.counter - action.value })
         // break;
         case actionTypes.RESET:
-            return { ...state, counter: 0 }
+            // return { ...state, counter: 0 }
+            return updateObject(state, { counter: 0 })
         // break;
 
         default:
--- src/store/reducers/results.js
+++ src/store/reducers/results.js
@@ -1,23 +1,31 @@
 import * as actionTypes from '../actions/actionType';
+import { updateObject } from '../utility';
 const initialState = {
     result: []
+}
+
+const deleteResult = (state, action) => {
+    const newArray = state.result.filter(result => result.id !== action.eleID)
+    return updateObject(state, { result: newArray })
 }
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         // change data here , like if you want to manipulate the result
         case actionTypes.STORE_RESULT:
-            return { ...state, result: state.result.concat({ id: new Date(), val: action.result }) } // we dont use push here cause then we will be pushing the original state and that is not advisable
-        // break;
+            // return { ...state, result: state.result.concat({ id: new Date(), val: action.result }) } // we dont use push here cause then we will be pushing the original state and that is not advisable
+            // break;
+            return updateObject(state, { result: state.result.concat({ id: new Date(), val: action.result }) })
         case actionTypes.DELETE_RESULT:
             // const id=2;
             // const newArray=[...state.result]  // this is best when you want to remove entire object from the array and not just the value
             // newArray.splice(id,1)
             // better way is to use filter as it returns a new array entirely
-            const newArray=state.result.filter( result => result.id !== action.eleID)
-            return {
-                ...state,
-                result:newArray
-            }
+            // const newArray=state.result.filter( result => result.id !== action.eleID)
+            // return {
+            //     ...state,
+            //     result:newArray
+            // }
+            return deleteResult(state, action);
         default:
             return state
         // break;
